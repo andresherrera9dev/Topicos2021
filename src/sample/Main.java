0f481c1c605e584220a82d30836f6028c3fb2610
andresherrera9dev <79882149+andresherrera9dev@users.noreply.github.com>
2021-03-07 23:10:28 -0600

    Tarea Implementacion de Paneles (actualizada)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
-package sample;
+//Alumno: Andres Herrera Oettinghaus
+//Numero de control: 19030566
 
+package sample;
 import javax.swing.*;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -8,8 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import java.util.Scanner;
 
 public class Main extends Application {
     private HBox obj_hBox,vBox;
@@ -18,44 +20,44 @@ public class Main extends Application {
     private BorderPane obj_borderpane;
     private GridPane obj_gridpane;
     private AnchorPane obj_anchorpane;
-    static Scanner cin=new Scanner(System.in);
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
+        primaryStage.setTitle("Proyecto de clase TAP 2021");
         UI_FlowPane();
         UI_BorderPane();
         UI_GridPane();
         UI_AnchorPane();
-        primaryStage.setTitle("Proyecto de clase TAP 2021");
 
         Integer input=Integer.parseInt(JOptionPane.showInputDialog("Eliga una opcion:\n\t1: Hbox." +
                 "\n\t2: BorderPane.\n\t3: Gridpane\n\t4: Anchorpane"));
         switch (input){
             case 1:
-                primaryStage.setScene(new Scene(obj_hBox, 300, 70));
+                primaryStage.setScene(new Scene(obj_hBox, 1800, 600));
                 break;
             case 2:
-                primaryStage.setScene(new Scene(obj_borderpane, 300, 70));
+                primaryStage.setScene(new Scene(obj_borderpane, 600, 700));
                 break;
             case 3:
-                primaryStage.setScene(new Scene(obj_gridpane, 300, 70));
+                primaryStage.setScene(new Scene(obj_gridpane, 600, 700));
                 break;
             case 4:
-                primaryStage.setScene(new Scene(obj_anchorpane, 300, 70));
+                primaryStage.setScene(new Scene(obj_anchorpane, 600, 700));
                 break;
             default:
                 break;
-
         }
         primaryStage.show();
     }
-
     public void UI_FlowPane(){
         btn1 = new Button("Boton 1");
+        btn1.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        btn1.setPrefSize(1000,400);
         btn2 = new Button("Boton 2");
+        btn2.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+        btn2.setPrefSize(1200,400);
         btn3 = new Button("Boton 3");
+        btn3.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        btn3.setPrefSize(1600,400);
 
         obj_hBox = new HBox();
         obj_hBox.getChildren().addAll(btn1, btn2, btn3);
@@ -65,10 +67,8 @@ public class Main extends Application {
         /*vBox = new HBox();
         vBox.getChildren().addAll(btn1, btn2, btn3);
         vBox.setPadding(new Insets( 20, 20, 20,  20));
-
          */
     }
-
     private void UI_BorderPane(){
         obj_borderpane = new BorderPane();
         obj_borderpane.setPrefSize(500,400);
@@ -78,7 +78,6 @@ public class Main extends Application {
         obj_borderpane.setRight(new TextField("Esta es una etiqueda a la Derecha"));
         obj_borderpane.setCenter(new TextField("Esta es una etiqueda en el centro"));
     }
-
     private void UI_GridPane(){
         obj_gridpane = new GridPane();
         obj_gridpane.add(new Button("Posicion 0, 0"), 0, 0);
@@ -86,21 +85,16 @@ public class Main extends Application {
         obj_gridpane.add(new Button("Posicion 0, 1"), 0, 1); obj_gridpane.add(new Button("Posicion 1, 1"), 1, 1);
         obj_gridpane.add(new Button("Posicion 0, 2"), 0, 2); obj_gridpane.add(new Button("Posicion 1, 2"), 1, 2);
     }
-
     private void UI_AnchorPane(){
         obj_anchorpane = new AnchorPane();
         anch_btn1 = new Button("Boton 1");
         anch_btn2 = new Button("Boton 2");
-
         AnchorPane.setTopAnchor(anch_btn1,15.0);
         AnchorPane.setRightAnchor(anch_btn1,15.0);
         AnchorPane.setBottomAnchor(anch_btn2,30.0);
         AnchorPane.setLeftAnchor(anch_btn2,100.0);
-
         obj_anchorpane.getChildren().addAll(anch_btn1,anch_btn2,new Button("Boton 3"));
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
